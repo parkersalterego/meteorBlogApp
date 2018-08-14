2cb2b9feaad0ff82efe95be954365c4cc7aa6b66
parkersalterego <parkersalterego92@gmail.com>
2018-08-13 17:32:33 -0700

    add post component added and triggering meteor method

--- imports/ui/NavBar.js
+++ imports/ui/NavBar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import history from '../routing/history';
+
+const navigateHome = () => {
+  history.push('/dashboard');
+}
 
 const NavBar = () => {
   return (
     <div className="navigation">
-      <h1 className="navigation__title">Meteor Blog App</h1>
+      <h1 onClick={navigateHome} className="navigation__title">Meteor Blog App</h1>
       <div className="link-container">
-          <NavLink className="link-container__link" exact to="/draftpost">New Post</NavLink>
+          <NavLink className="link-container__link" exact to="/newpost">New Post</NavLink>
       </div>
     </div>
   )
