0e961496f858b5ab70dddfac82635c1c1c36116b
parkersalterego <parkersalterego92@gmail.com>
2018-08-16 00:32:25 -0700

    authenticated and unauthenticated route tracking added

--- imports/ui/NavBar.js
+++ imports/ui/NavBar.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react'
 import history from '../routing/history';
-
-const navigateHome = () => {
-  history.push('/dashboard');
-}
 
 const NavBar = () => {
   return (
     <div className="navigation">
-      <h1 onClick={navigateHome} className="navigation__title">Meteor Blog App</h1>
-      <div className="link-container">
-          <NavLink className="link-container__link" exact to="/newpost">New Post</NavLink>
-      </div>
+      <h1 onClick={() => history.push('/dashboard')} className="navigation__title">Meteor Blog App</h1>
+      <ul className="link-container">
+        <li onClick={()=> history.push('/newpost')}>New Post</li>
+        <li onClick={() => {
+          Meteor.logout(() => {
+            history.push('/');
+          });
+        }}>logout</li>
+      </ul>
     </div>
   )
 }
