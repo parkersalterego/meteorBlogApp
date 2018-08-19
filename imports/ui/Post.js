87a3d6d73cf2f1c61898edb39a8a15a24c1ac984
parkersalterego <parkersalterego92@gmail.com>
2018-08-19 11:58:59 -0700

    moment pulled in and dates formatted

--- imports/ui/Post.js
+++ imports/ui/Post.js
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import { Meteor } from 'meteor/meteor';
 import FlashMessages from './FlashMessages';
 import history from '../routing/history';
+import moment from 'moment';
 
 class Post extends Component {
 
@@ -77,7 +78,7 @@ class Post extends Component {
                     <button onClick={() => this.setState({edit: true})}>Edit</button>
                     <button onClick={this.deletePost.bind(this)}>Delete</button>
                     {/* use moment js below */}
-                    <p className="post__created-at">{this.state.post.createdAt}</p>
+                    <p className="post__created-at">{moment(this.state.post.createdAt).format('MMMM Do, YYYY')}</p>
                     <br/>
                     <p className="post__body" ref="postBody">{this.state.post.body}</p>
                 </div>
